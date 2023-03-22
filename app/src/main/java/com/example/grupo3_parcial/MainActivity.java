fb34e50b915cf353329347257dfda45f7929be00
Faherme46 <faherme46@gmail.com>
2023-03-22 15:28:54 -0500

    CrearAdapter

--- app/src/main/java/com/example/grupo3_parcial/MainActivity.java
+++ app/src/main/java/com/example/grupo3_parcial/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.grupo3_parcial.clases.partido;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<partido> listaPartidos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Partidos de la Seleccion");
+
+
     }
 }
